627d8dee2badf407161bd057da4c834071839cc8
Shahzad Bhatti <bhatti@plexobject.com>
2023-03-15 17:26:30 -0700

    updated swagger docs

--- internal/proxy/proxy_handler.go
+++ internal/proxy/proxy_handler.go
@@ -260,7 +260,11 @@ func (h *Handler) doHandleResponse(resp *http.Response, _ *goproxy.ProxyCtx) (*h
 	resp.Header["Access-Control-Max-Age"] = []string{"1728000"}
 	resp.Header["Content-Length"] = []string{"0"}
 	resp.Header["Access-Control-Expose-Headers"] = []string{"Content-Length,Content-Range"}
-	resp.Header["Via"] = []string{h.config.UserAgent}
+	agent := h.config.UserAgent
+	if resp.Header.Get("Via") != "" {
+		agent = agent + " (" + resp.Header.Get("Via") + ")"
+	}
+	resp.Header["Via"] = []string{agent}
 	//resp.Header["Vary"] = []string{"Origin, Accept-Encoding""}
 	//resp.Header["Access-Control-Allow-Headers"] = []string{"Content-Type, api_key, Authorization"}
 	//resp.Header["Content-Security-Policy"] = []string{"default-src 'self', form-action 'self',script-src 'self'"}
